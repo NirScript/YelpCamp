24eb80cad8bef09e618d42c97b4e8b8f02bc7f9f
Nir <nirnir122@gmail.com>
2018-03-11 19:30:00 +0200

    update dburl

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index")
     
 /* mongoose.connect("mongodb://localhost/yelp_camp_v11"); */
-mongoose.connect("mongodb://nir:lolyou1@ds211289.mlab.com:11289/yelpcamp");
+mongoose.connect(process.env.DATABASEURL);
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
