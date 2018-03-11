867bf705dd786eabf22a032cd555e1f8637ba9f1
Nir <nirnir122@gmail.com>
2018-03-11 17:31:12 +0200

    changed listen port

--- app.js
+++ app.js
@@ -48,6 +48,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(3000, function(){
-   console.log("Server is online");
-});
+app.listen(process.env.PORT || 3000, function() {
+    console.log("listening on 3000");
+  });
