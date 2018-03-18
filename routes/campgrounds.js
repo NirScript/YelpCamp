eb26353a0bd23521122bc69382dec408bdf5d5aa
Nir <nirnir122@gmail.com>
2018-03-18 19:17:23 +0200

    add fuzzy search

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -6,14 +6,29 @@ var middleware = require("../middleware");
 
 //INDEX ROUTE
 router.get("/", function(req, res){
-    // Get all camps from db
-    Campground.find({}, function(err, allCampgrounds){
-       if(err){
-           console.log(err);
-       } else {
-          res.render("campgrounds/index",{campgrounds: allCampgrounds, page: 'campgrounds'});
-       }
-    });
+    var noMatch = null;
+    if(req.query.search){
+        const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+        Campground.find({name:regex}, function(err, allCampgrounds){
+            if(err){
+                console.log(err);
+            } else {
+                if(allCampgrounds < 1) {
+                    noMatch = "No campgrounds match that query, please try another one";
+                }
+               res.render("campgrounds/index",{campgrounds: allCampgrounds, noMatch: noMatch});
+            }
+         });
+    } else {
+        //get all camps from db
+        Campground.find({}, function(err, allCampgrounds){
+            if(err){
+                console.log(err);
+            } else {
+                res.render("campgrounds/index", {campgrounds: allCampgrounds, noMatch: noMatch});
+            }
+        });
+    }
 });
 
 //CREATE ROUTE
@@ -91,6 +106,9 @@ router.delete("/:id",middleware.checkCampgroundOwnership, function(req, res){
    });
 });
 
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
 
 module.exports = router;
 
